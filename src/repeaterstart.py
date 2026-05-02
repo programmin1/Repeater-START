034904b1dec80c7253fa5af126702481f19dae8b
lb1programmer <luke@linuxpc>
2026-05-02 08:43:29 -0700

    Debian files for 1.1 beta

--- src/repeaterstart.py
+++ src/repeaterstart.py
@@ -131,7 +131,7 @@ class BackgroundDownloadZip(BackgroundDownload):
 class UI(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, type=Gtk.WindowType.TOPLEVEL)
-        self.version = '1.0.3'
+        self.version = '1.1-beta'
         self.mode = ''
         self.set_default_size(600, 600)
         self.connect('destroy', self.cleanup)
