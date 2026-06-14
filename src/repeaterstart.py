6e8076032636c323c09da3d09ade9fb0f3f41e35
programmin1 <lb1programmer@users.sourceforge.net>
2026-06-14 07:39:32 -0700

    Version 1.1.0

--- src/repeaterstart.py
+++ src/repeaterstart.py
@@ -155,7 +155,7 @@ class BackgroundDownloadZip(BackgroundDownload):
 class UI(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, type=Gtk.WindowType.TOPLEVEL)
-        self.version = '1.1-beta3'
+        self.version = '1.1.0'
         self.mode = ''
         self.set_default_size(600, 600)
         self.connect('destroy', self.cleanup)
